a81820721d71751b4b0f2a0694099aad6db436e4
comptergeeks <F@rhan0108>
2024-03-16 20:01:53 -0700

    integrated app with react so much easier

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+    </div>
+  );
+}
+
+export default App;
